63ed808e2d690d3598efa69e801bc4f8ecc8f5dd
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-12-11 21:10:40 +0200

    add task 1

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+const list = document.querySelector('#categories');
+console.dir(list.children);
+
+const items = [...list.children];
+
+const quantity = items.length
+console.log('Number of categories:', quantity);
+
+
+items.forEach((item) => {
+    console.log('Category:', item.firstElementChild.textContent);
+    console.log('Elements:', item.lastElementChild.childElementCount);
+  });
