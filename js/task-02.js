d0eab0f5f5fcc26d0fd901eea7bc077256b11e8b
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-12-12 14:36:04 +0200

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+list.textContent = 'ingredients';
+console.dir(list);
+
+const markup = ingredients.map(ingredient => {
+  const liEl = document.createElement("li");
+  liEl.classList.add('item');
+  liEl.textContent = ingredient;
+  return liEl
+  
+})
+list.append(...markup);
+console.dir(markup);
