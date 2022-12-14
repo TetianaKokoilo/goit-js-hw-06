93d309ce898cad21c549a5a03349d5f3a8be576b
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-12-14 14:24:32 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,44 +1,21 @@
 const form = document.querySelector('.login-form');
+
 console.dir(form);
 
 form.addEventListener('submit', onSubmit);
-const validateForm = true;
-function onSubmit(event) {
-  event.preventDefault();
-  if (document.login-form == '') {
-    alert('Всі поля повинні бути заповнені!');
-    validateForm = false;
+function onSubmit(evt) {
+
+  evt.preventDefault();
+
+  const { email, password } = evt.currentTarget.elements;
+  console.log('email', email.value);
+  console.log('password', password.value);
+
+    if (email.value || password.value === '' ) {
+   alert ( "Всі поля повинні бути заповнені." );
+    } else {
+      return evt.currentTarget.elements
   }
-
+  form.reset()
   
-  const formData = new FormData(event.currentTarget);
-
-  console.log(formData);
-
-  formData.forEach((value, name) => {
-    console.log(`name`, name);
-    console.log(`value`, value);
-  });
-    return validateForm;
-
-  //   document.getElementById(".login-form").reset();
 }
-// function validate_form() {
-//   valid = true;
-
-//   if (document.login - form.email.password.value == '') {
-//     alert('Все поля должны быть заполнены.');
-//     valid = false;
-//   }
-
-//   return valid;
-// }
-
-// function onSubmit(evt) {
-//     evt.preventDefault()
-
-//     const {email, password} = evt.currentTarget.elements;
-//     console.log('email', email.value);
-//     console.log('password', password.value);
-
-// }
