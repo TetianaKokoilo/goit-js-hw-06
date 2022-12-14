129ed455b6d3a0bd12ab762b68eb802af6e62fe3
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-12-14 13:10:51 +0200

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const changeColor = document.querySelector('.change-color');
+const textColor = document.querySelector('.color');
+// console.dir(changeColor);
+// console.dir(textColor);
+
+changeColor.addEventListener('click', () => {
+
+  let color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  textColor.textContent = color;
+})
+
+
+function getRandomHexColor () {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
